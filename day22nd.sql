4e79244bae0e09aede4d942d42d491b2f68ebadd
suraj-tiwary18 <surajtiwari9308@gmail.com>
2026-06-19 20:33:25 +0530

    Adding 22nd Day SQL file

---
+++ day22nd.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE surajtiwari;
+
+-- USE surajtiwari;
+
+-- CREATE TABLE Students (
+--     StudentID INT AUTO_INCREMENT PRIMARY KEY,
+--     Name VARCHAR(100),
+--     Age TINYINT,
+--     Email VARCHAR(100),
+--     JoinDate DATE
+-- );
+
+-- INSERT INTO Students (Name, Age, Email, JoinDate)
+-- VALUES ('Suraj Tiwari', 20, 'suraj@gmail.com', '2026-06-19');
+
+-- SELECT * FROM Students;
