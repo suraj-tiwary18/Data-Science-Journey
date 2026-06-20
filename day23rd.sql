4ea773d628984389f57c4c8c2897c51303fc8ae3
suraj-tiwary18 <surajtiwari9308@gmail.com>
2026-06-20 21:53:35 +0530

    Add Day 23rd file

---
+++ day23rd.sql
@@ -0,0 +1,67 @@
+-- What is MySQL?
+-- MySQL is a very popular open-source relational database management system (RDBMS).
+
+-- MySQL is open-source
+-- MySQL is free
+-- MySQL is ideal for both small and large applications
+-- MySQL is very fast, reliable, scalable, and easy to use
+-- MySQL is cross-platform
+-- MySQL is compliant with the ANSI SQL standard
+-- MySQL was first released in 1995
+-- MySQL is developed, distributed, and supported by Oracle Corporation
+-- MySQL is named after co-founder Ulf Michael "Monty" Widenius's daughter: My
+
+
+
+
+--  The Most Important SQL Commands
+-- SELECT - extracts data from a database
+-- UPDATE - updates data in a database
+-- DELETE - deletes data from a database
+-- INSERT INTO - inserts new data into a database
+-- CREATE DATABASE - creates a new database
+-- ALTER DATABASE - modifies a database
+-- CREATE TABLE - creates a new table
+-- ALTER TABLE - modifies a table
+-- DROP TABLE - deletes a table
+-- CREATE INDEX - creates an index (search key)
+-- DROP INDEX - deletes an index
+
+
+
+
+-- Today's class :-
+
+-- create database surajtiwari;
+
+-- use surajtiwari;
+
+-- show tables;
+
+-- create table user(name varchar(30)); 
+
+-- show tables;
+
+-- insert into user values("hello");
+
+-- select * from user;
+
+-- SET SQL_SAFE_UPDATES = 0;
+
+-- update user set name = 'surajkumar' where name = 'hello';
+
+-- select * from user;
+
+-- delete from user where name = 'surajtiwari';
+
+-- select * from user;
+
+-- drop table user;
+
+-- show tables;
+
+-- drop database surajtiwari;
+ 
+-- use dumpdata;
+
+-- select * from user;
