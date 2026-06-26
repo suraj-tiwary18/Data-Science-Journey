5c8010de3a8d4b0f29b5e0fe8721b54ab87ab99b
suraj-tiwary18 <surajtiwari9308@gmail.com>
2026-06-27 01:05:58 +0530

    Modified Day 28th folder

--- day28th_ML_Project/project.py
+++ day28th_ML_Project/project.py
@@ -7,26 +7,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-# #pymysql connector
-# import pymysql
-# conn = pymysql.connect(host="13.220.156.88",user="mluser",database="ml_db",password="mlpass123")
-# print(conn)
-# query = "select avg_income,house_age,num_rooms,price, name from houses"
-# df = pd.read_sql(query, conn)
+#pymysql connector
+import pymysql
+conn = pymysql.connect(host="localhost",user="root",database="day28th_db",password="2005")
+print(conn)
+query = "select avg_income,house_age,num_rooms,price, name from houses"
+df = pd.read_sql(query, conn)
 # print(df)
 
 
-# Static data
-data = {
-    "name": ['suraj', 'sushant', 'shivam', 'riya'],
-    "avg_income": [1000,200,300,400], # hundred and thousand
-    "house_age": [10.2, 2.5, 3.0, 4.0],
-    "num_rooms": [3,4,5,1],
-    "price": [10000,7000,15000,11000]
-}
+# # Static data
+# data = {
+#     "name": ['suraj', 'sushant', 'shivam', 'riya'],
+#     "avg_income": [1000,200,300,400], # hundred and thousand
+#     "house_age": [10.2, 2.5, 3.0, 4.0],
+#     "num_rooms": [3,4,5,1],
+#     "price": [10000,7000,15000,11000]
+# }
 
-# Data frame
-df = pd.DataFrame(data)
+# # Data frame
+# df = pd.DataFrame(data)
 
 # handle missing values
 df.drop('name', axis=1, inplace = True)
@@ -47,21 +47,24 @@ x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_st
 model = LinearRegression()
 model.fit(x_train, y_train)
 
-# Prediction 
+# Prediction
+avg = int(input("Enter Avg. income: "))
+age = int(input("Enter age of house: "))
+rooms = int(input("Enter rooms in the house: "))
+
 new_data = pd.DataFrame({
-    "avg_income": [900],
-    "house_age": [8],
-    "num_rooms": [5]
+    "avg_income": [avg],
+    "house_age": [age],
+    "num_rooms": [rooms]
 })
 
 # Feature Scaling
-# new_data[['avg_income', 'house_age', 'num_rooms']] = scaler.fit_transform(new_data[['avg_income', 'house_age', 'num_rooms']])
+new_data[['avg_income', 'house_age', 'num_rooms']] = scaler.transform(new_data[['avg_income', 'house_age', 'num_rooms']])
 
 # Prediction
 predict_data = model.predict(new_data)
-print(f"Predicted value is : {predict_data[0]}")
+print(f"Predicted value is : ₹{predict_data[0]:,.2f}")
 
 # Model Dump
 import joblib
 joblib.dump(model, "house_model.joblib")
-
